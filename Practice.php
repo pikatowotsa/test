31ece8a109ed598e2295960d3cdbc202bdcbf7f3
naga <pikato@gmail.com>
2024-06-02 23:06:52 +0530

    file modified

--- Practice.php
+++ Practice.php
@@ -4,15 +4,5 @@ require_once "autoload.php";
 use \Include\MyInterface;
 use \Include\MyClassB;
 
-$arr = [
-    [ "name" => "Donald"],
-    ["age" => 34]
-];
 
-foreach($arr as $newArr)
-{
-     foreach($newArr as $arrItem)
-     {
-        echo $arrItem ."\n";
-     }
-}
+print_r(glob("*"));
